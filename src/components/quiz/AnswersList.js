68466ca9dba001d65e136a82d88352d1c6e2e06f
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-15 14:10:35 +0100

    answers list

---
+++ src/components/quiz/AnswersList.js
@@ -0,0 +1,98 @@
+import { useRef, useState, useEffect } from "react";
+import LIST from "../../utils/list2";
+function AnswersList({idx, shuffle, setNextQuestion, skipQuestion, setTimerBlocked, showCorrect}){
+
+    const answers = useRef(null);
+
+    const correct = useRef(null);
+
+    const chosenAnswerRef = useRef(null);
+
+    const [mode, setMode] = useState(null);
+    
+    if(correct.current === null){
+        correct.current = LIST[idx].correct;
+    }
+
+    if(answers.current === null){
+
+        const rawAnswers = LIST[idx].answers;
+        const readyAnswers = [...Object.entries(rawAnswers)];
+
+        if(shuffle){
+            readyAnswers.sort(() => Math.random() - 0.5);
+        }
+        answers.current = [...readyAnswers];
+    }
+
+    useEffect(() => {
+        let timer;
+        switch(mode){
+            case null:
+                break;
+            case 'chosen':
+                chosenAnswerRef.current.className = 'selectedAnswer';
+                if(showCorrect === false){
+                    setTimerBlocked(true);
+                    timer = setTimeout(() => {
+                        setNextQuestion(chosenAnswerRef.current.dataset.idx);
+                    },700)
+                    break;
+                }
+                
+                setMode('waiting');
+                setTimerBlocked(true);
+                break;
+            case 'waiting':
+                timer = setTimeout(() => {
+                let cls = chosenAnswerRef.current.dataset.idx === correct.current ? 'correctAnswer' : 'wrongAnswer';
+                chosenAnswerRef.current.className = cls;
+                setMode('feedbackSent')
+                },2000);
+                break;
+            case 'feedbackSent':
+                timer = setTimeout(() => {
+                    setNextQuestion(chosenAnswerRef.current.dataset.idx);
+                },1000);
+                break;
+            default:
+                break;
+        }
+
+        return () => {
+            clearTimeout(timer);
+          };
+
+    }, [mode, setNextQuestion, setTimerBlocked, showCorrect]);
+
+
+    function handleClick(e){
+        if(chosenAnswerRef.current === null){
+            chosenAnswerRef.current = e.target;
+            setMode('chosen');
+        }
+    }
+    
+
+    return (
+        <> 
+            <ul>
+            {answers.current.map((item) => {
+                const [key,value] = item;
+               return (
+               <li 
+               key={key} 
+               data-idx={key} 
+               onClick={(e) => handleClick(e)}
+               >{value}</li>)
+            })}
+            </ul>
+            <button 
+            disabled={mode !== null}
+            onClick={() => skipQuestion()}
+            >Skip</button>
+        </>
+    );
+}
+
+export {AnswersList};
