78b20167e3d3da229578941fb4e79ca64b49130a
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-15 14:17:56 +0100

    another form rfc

---
+++ src/components/form/ShowScoreCheckbox.js
@@ -0,0 +1,16 @@
+import { useQuizWithDispatch } from "../../context/quizcontext";
+function ShowScoreCheckbox(){
+    const [{showScore}, dispatch] = useQuizWithDispatch();
+    return (
+        <div>
+        <input 
+        type="checkbox" 
+        id="showScore" 
+        checked={showScore}
+        onChange={() => dispatch({type: 'toggleShowScore'})} />
+        <label htmlFor="showScore">Show score: {showScore ? 'YES' : 'NO'}</label>
+        </div>
+    )
+}
+
+export {ShowScoreCheckbox};
