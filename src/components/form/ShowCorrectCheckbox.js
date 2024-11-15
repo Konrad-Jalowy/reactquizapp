21d65cecefea9e94271f6676af315398a2f20be2
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-15 14:17:23 +0100

    form rfc

---
+++ src/components/form/ShowCorrectCheckbox.js
@@ -0,0 +1,17 @@
+import { useQuizWithDispatch } from "../../context/quizcontext";
+
+function ShowCorrectCheckbox(){
+    const [{showCorrect}, dispatch] = useQuizWithDispatch();
+    return (
+        <div>
+        <input 
+        type="checkbox" 
+        id="showCorrect" 
+        checked={showCorrect}
+        onChange={() => dispatch({type: 'toggleShowCorrect'})} />
+        <label htmlFor="showCorrect">Show correct: {showCorrect ? 'YES' : 'NO'}</label>
+        </div>
+    )
+}
+
+export {ShowCorrectCheckbox};
