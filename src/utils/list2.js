4dcf574e82037088ec35aea1d144c6862c1440ed
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-15 13:50:52 +0100

    question list

---
+++ src/utils/list2.js
@@ -0,0 +1,35 @@
+const LIST = [
+    {
+        question: "What is 4 - 2 ?",
+        answers: {
+            optionA: "2",
+            optionB: "4",
+            optionC: "8",
+            optionD: "10"
+        },
+        correct: "optionA"
+    },
+    {
+        question: "What is 4 * 2?",
+        answers: {
+            optionA: "2",
+            optionB: "4",
+            optionC: "8",
+            optionD: "10"
+        },
+        correct: "optionC"
+    },
+    {
+        question: "What is 4 - 2?",
+        answers: {
+            optionA: "1",
+            optionB: "4",
+            optionC: "2",
+            optionD: "10"
+        },
+        correct: "optionC"
+    }
+
+]
+
+export default LIST;
