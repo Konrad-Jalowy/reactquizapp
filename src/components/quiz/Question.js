4441cb012c015c8bf728d9ae4c87c52a6ef02719
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-16 11:17:03 +0100

    css styles

--- src/components/quiz/Question.js
+++ src/components/quiz/Question.js
@@ -1,9 +1,9 @@
 import LIST from "../../utils/list2";
-
+import './Question.css';
 function Question({idx}){
 
     return (
-        <h2>{LIST[idx].question}</h2>
+        <h2><span className="index">Question {idx+1}:</span> <em>{LIST[idx].question}</em></h2>
     )
 }
 
