6b3e46c97fed243c644e4fc79f6fd1c0c05589ac
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-16 12:40:58 +0100

    css styles

--- src/components/form/QuestionTiming.js
+++ src/components/form/QuestionTiming.js
@@ -14,24 +14,28 @@ function QuestionTiming(){
     }
 
     return (
-        <div>
+        <>
+        <div className="checkbox">
         <input 
         type="checkbox" 
         id="timedQuestions" 
         checked={timedQuestions} 
         onChange={onTimedQestionsChange}  />
         <label htmlFor="timedQuestions">Timed Questions: {timedQuestions ? 'YES' : 'NO'}</label>
-        <p></p>
-        <label htmlFor="questionTime">Question Time:</label>
-        <p></p>
+        </div>
+        <div className="qTime">
+        <label htmlFor="questionTime">Question Time: {timePerQuestion}s</label>
+        
         <input 
         disabled={timedQuestions ? false: true}
         value={timePerQuestion} 
         onChange={onQuestionTimeChange} 
         type="range" 
         id="questionTime"  
-        min="5" max="15" /> {timePerQuestion}s
+        min="5" max="15" /> 
         </div>
+        </>
+        
     )
 }
 
